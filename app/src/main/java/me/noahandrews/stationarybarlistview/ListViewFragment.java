32c28a1eb8572ac188b094d19c47bcd2a03d2b51
NoahAndrews <noah@moeftc.org>
2015-02-20 14:55:56 -0500

    Really added the Fragment files

---
+++ app/src/main/java/me/noahandrews/stationarybarlistview/ListViewFragment.java
@@ -0,0 +1,53 @@
+package me.noahandrews.stationarybarlistview;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.github.ksoichiro.android.observablescrollview.ObservableListView;
+import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
+import com.github.ksoichiro.android.observablescrollview.ScrollUtils;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ListViewFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link ListViewFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ListViewFragment extends Fragment {
+    private static final String ARG_INITIAL_POSITION = "initialPosition";
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_listview, container, false);
+        Activity parentActivity = getActivity();
+        final ObservableListView listView = (ObservableListView) view.findViewById(R.id.scroll);
+        //In example program, setDummyDataWithHeader is called here.
+        if (parentActivity instanceof ObservableScrollViewCallbacks) {
+            Bundle args = getArguments();
+            if (args != null && args.containsKey(ARG_INITIAL_POSITION)) {
+                final int initialPosition = args.getInt(ARG_INITIAL_POSITION, 0);
+                ScrollUtils.addOnGlobalLayoutListener(listView, new Runnable() {
+                    @Override
+                    public void run() {
+                        listView.setSelection(initialPosition);
+                    }
+                });
+            }
+            listView.setScrollViewCallbacks((ObservableScrollViewCallbacks) parentActivity);
+        }
+        return view;
+
+
+    }
+}
